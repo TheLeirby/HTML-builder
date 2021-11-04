06269b0eb79f1ee7ada34329ee649cc9445dc196
Vasiliy <Vasiliy@6664.ru>
2021-11-04 17:20:23 +0300

    init:05 merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,21 +2,22 @@ const fs = require('fs');
 const path = require('path');
 const filesDirectory = path.join(__dirname, 'styles');
 const bundlePath = path.join(__dirname, 'project-dist/bundle.css');
-let filesForMessage='';
+const process = require('process');
+let message='';
 fs.readdir(filesDirectory, {withFileTypes: true}, function (error, items) {    
     if (!error){        
         items.forEach(i => {            
             findStyles(i.name);
         });        
     } else {
-        console.log('Error\x1b[35m', error);
+        message=`Error\x1b[31m ${error}`;
     }
 });
 fs.writeFile(bundlePath, '', (error) => {
     if (!error){        
-        filesForMessage+=`\x1b[33mCopy styles to bundle.css from file: \x1b[37m\t| `;
+        message+=`\x1b[33mCopy styles to bundle.css from file: \x1b[37m\t| `;
     } else{
-        console.log('Error\x1b[35m', error);
+        message=`Error\x1b[31m ${error}`;
     }
 });
 function findStyles(file){
@@ -27,17 +28,17 @@ function findStyles(file){
         if (!error){        
             fs.appendFile(bundlePath, data, (error) => {
                 if (!error){        
-                    filesForMessage+=`\x1b[32m${file}\x1b[37m |\t`;                    
+                    message+=`\x1b[32m${file}\x1b[37m |\t`;                    
                 } else{
-                    console.log('Error\x1b[35m', error);
+                    message=`Error\x1b[31m ${error}`;
                 }
             });
         } else{
-            console.log('Error\x1b[35m', error);
+            message=`Error\x1b[31m ${error}`;
         }      
     });
   } else {
-    console.log(`\x1b[31mSorry file:${file}, is not for CSS\x1b[37m`)
+    message+=`\x1b[31mSorry file:${file}, is not for CSS\x1b[37m\n`;
   }
 }
-const process = require('process');process.on('beforeExit', () => {console.log(filesForMessage);});
+process.on('beforeExit', () => {console.log(message);});
